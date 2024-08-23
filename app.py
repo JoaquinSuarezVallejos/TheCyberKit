ad23aeea1dac0e4e52c653cf0907fc4f693c5341
Joaquín <joaquin.suarezvallejos@gmail.com>
2024-08-23 20:42:46 -0300

    added tailwind, node, npm,postcss, etc

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+# To serve project: 1. flask_env\Scripts\activate | 2. python app.py
+
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', static_url_path='/static')
 
 @app.route('/')
 def index():
