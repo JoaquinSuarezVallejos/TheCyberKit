d38291e9dbb4a4a90ea856cdcca2163bfa5431d1
Joaquín <joaquin.suarezvallejos@gmail.com>
2024-08-26 21:26:03 -0300

    minor color changes and kebab-case changes

--- static/js/script.js
+++ static/js/script.js
@@ -2,7 +2,7 @@
 /* Case types used: camelCase (for functions and variables), 
 SCREAMING_SNAKE_CASE (for constants) and kebab-case (for CSS classes) */
 
-const CYBERKIT_TITLE_TEXT = document.getElementById('cyberkit_title_text');
+const CYBERKIT_TITLE_TEXT = document.getElementById('cyberkit-title-text');
 const ORIGINAL_TEXT = CYBERKIT_TITLE_TEXT.textContent;
 
 function transitionToModifiedText() {
