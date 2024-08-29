32df9d45b170f2f5cf88db0fff1333df820c5435
Joaquín <joaquin.suarezvallejos@gmail.com>
2024-08-28 21:19:36 -0300

    added .gitignore and minor changes

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # PYTHON FILE
 # Case types used: snake_case (for functions and variables) and SCREAMING_SNAKE_CASE (for constants)
 
-# Commands to serve Flask project: 1. flask_env\Scripts\activate | 2. python app.py
+# Commands to serve Flask project: 1. flask_env\Scripts\activate | 2. python app.py (or python3 app.py)
 
 from flask import Flask, render_template
 
