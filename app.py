9bf8253a5d2e24f28f849252fddccb0da087c3f6
Joaquín <joaquin.suarezvallejos@gmail.com>
2024-08-30 20:32:55 -0300

    added setup instructions (app.py)

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 # PYTHON FILE
 # Case types used: snake_case (for functions and variables) and SCREAMING_SNAKE_CASE (for constants)
 
-# Commands to serve Flask project on Windows: 1. flask_env\Scripts\activate | 2. python app.py (or python3 app.py)
+# Setup for Windows (use PowerShell or the VS Code terminal): 
+# 1. Clone the repository from GitHub: https://github.com/JoaquinSuarezVallejos/TheCyberKit/tree/main
+# 2. Create the Flask virtual environment: python -m venv flask_env
+# 3. Activate the virtual environment: flask_env\Scripts\activate
+# 4. Install dependencies from the requirements.txt file: pip install -r requirements.txt
+
+# Commands to serve Flask project: 1. flask_env\Scripts\activate | 2. python app.py (or python3 app.py)
 
 from flask import Flask, render_template
 
@@ -14,6 +20,4 @@ def index():
 if __name__ == '__main__':
     app.run(debug=True)
     
-# TODO: Create the requirements.txt file. Ask Gemini if this command works: pip freeze > requirements.txt
-
 # TODO: Start developing here.
