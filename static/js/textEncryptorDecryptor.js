59fd680dd2e9c858a88a76efacc0e698d0a32300
Joaquín <joaquin.suarezvallejos@gmail.com>
2024-11-24 16:27:03 -0300

    working on the encryptor/decryptor

--- static/js/textEncryptorDecryptor.js
+++ static/js/textEncryptorDecryptor.js
@@ -11,10 +11,21 @@ const inputLabel2 = document.querySelector(".encryption-decryption-input-label2"
 const outputLabel = document.querySelector(
   ".encryption-decryption-output-label"
 );
+const encryptionDecryptionTextAreaInput = document.getElementById("encryption-decryption-textarea-input");
+const customKeyInputBox = document.querySelector(".custom-key-input-box");
+const encryptionDecryptionTextAreaOutput = document.getElementById("encryption-decryption-textarea-output");
+
 
 // Listen for radio button changes
 encryptRadioBtn.addEventListener("change", updateAction);
 decryptRadioBtn.addEventListener("change", updateAction);
+
+
+function resetFields() {
+  encryptionDecryptionTextAreaInput.value = "";
+  encryptionDecryptionTextAreaOutput.value = "";
+  customKeyInputBox.value = "";
+}
 
 // Update action button, labels, and placeholder based on selected radio button
 function updateAction() {
@@ -23,16 +34,19 @@ function updateAction() {
     inputLabel.textContent = "Enter text to encrypt:";
     inputLabel2.textContent = "Encrypt with a custom secret key:"
     outputLabel.textContent = "Encrypted text output:";
+    resetFields()
   } else if (decryptRadioBtn.checked) {
     encryptDecryptSubmitBtn.textContent = "Decrypt";
     inputLabel.textContent = "Enter text to decrypt:";
     inputLabel2.textContent = "Decryption requires a custom secret key:"
     outputLabel.textContent = "Decrypted text output:";
+    resetFields()
   } else {
     // Default to encrypt
     encryptDecryptSubmitBtn.textContent = "Encrypt";
     inputLabel.textContent = "Enter text to encrypt:";
     inputLabel2.textContent = "Encrypt with a custom secret key:"
     outputLabel.textContent = "Encrypted text output:";
+    resetFields()
   }
 }
